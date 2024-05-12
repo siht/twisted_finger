f8ee46e13d2d83fb3dba6804a9562eb6b2966ca4
Carlos Tzompantzi <carlos.tz@wayssen.com.mx>
2024-05-11 21:03:18 -0600

    finger 06

--- finger.py
+++ finger.py
@@ -8,20 +8,22 @@ from twisted.internet import (
 from twisted.protocols import basic
 
 
-class FingerProtocol(basic.LineReceiver): # LineReceiver es un protocolo que ya tiene los métodos para recibir texto del cliente
-    def lineReceived(self, user): # esta vez no estamos dropeando la conexion inmediatamente, ahora esperamos input del cliente
-        self.transport.write(b'No such user\r\n') # hacemos una respuesta mock, nunca hay usuarios
+class FingerProtocol(basic.LineReceiver):
+    def lineReceived(self, user): # ahora nuestra fuente de los datos es el factory
+        self.transport.write(self.factory.getUser(user) + b'\r\n')
         self.transport.loseConnection()
 
 
 class FingerFactory(protocol.ServerFactory):
-    protocol = FingerProtocol
+    protocol = FingerProtocol # cuando registramos el protocolo dentro del factory se crea una propiedad dentro del protocol para acceder
 
+    def getUser(self, user):
+        return b'No such user'
 
 def main():
     # qué hace esto? al abrir una consola y escribir `telnet localhost 1079`
     # escribes el nombre de un usuario y te regresa: No such user
-    # un servicio finger util para trolear
+    # no ha cambiado, sólo que ahora obtenemos los datos del factory
     fingerEndpoint = endpoints.serverFromString(reactor, 'tcp:1079')
     fingerEndpoint.listen(FingerFactory())
     reactor.run()
