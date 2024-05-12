1ccddf5bf3c43b07b42b8bca5bc679e933a9315a
Carlos Tzompantzi <carlos.tz@wayssen.com.mx>
2024-05-11 21:07:36 -0600

    finger 07

--- finger.py
+++ finger.py
@@ -17,15 +17,21 @@ class FingerProtocol(basic.LineReceiver):
 class FingerFactory(protocol.ServerFactory):
     protocol = FingerProtocol # cuando registramos el protocolo dentro del factory se crea una propiedad dentro del protocol para acceder
 
+    def __init__(self, users):
+        self.users = users
+
     def getUser(self, user):
-        return b'No such user'
+        return self.users.get(user, b'No such user')
+
 
 def main():
     # qué hace esto? al abrir una consola y escribir `telnet localhost 1079`
-    # escribes el nombre de un usuario y te regresa: No such user
-    # no ha cambiado, sólo que ahora obtenemos los datos del factory
+    # escribes el nombre de un usuario moshez regresa: Happy and well
+    # cualquier otro nombre te regresa: No such user
+    # ahora ya hay una fuente de datos intercambiable y tenemos nuestra primera db (un diccionario)
+    # ahora ya empieza parecerse a finger
     fingerEndpoint = endpoints.serverFromString(reactor, 'tcp:1079')
-    fingerEndpoint.listen(FingerFactory())
+    fingerEndpoint.listen(FingerFactory({b'moshez': b'Happy and well'}))
     reactor.run()
 
 
