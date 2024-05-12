bb7b77ea79b0ba73c5daf0e32ea4c4a7e645ff9e
Carlos Tzompantzi <carlos.tz@wayssen.com.mx>
2024-05-11 20:41:52 -0600

    finger 02

--- finger.py
+++ finger.py
@@ -1,12 +1,29 @@
 # como correr?
 # python finger.py
-from twisted.internet import reactor
+from twisted.internet import (
+    endpoints,
+    protocol,
+    reactor,
+)
+
+
+class FingerProtocol(protocol.Protocol): # basicamente su trabajo es escuchar y enviar, y tener una lógica de protocolo no de negocio
+    pass
+
+
+class FingerFactory(protocol.ServerFactory): # su trabajo es ofrecer lógica de negocio y datos
+    protocol = FingerProtocol
 
 
 def main():
-    # para que twisted o cualquier cosa que funcione asincronamente debe haber
-    # un "event manager" o un "reactor" digamos que es un while que maneja eventos
-    # que hace esto? nada, no hay lógica, sólo estamos poniendo la estructura
+    # como funciona twisted Enpoint(Factory(Protocol))
+    # resumiendo de manera sencilla necesitas un punto de acceso (Endpoint)
+    # que escuche un factory que a su vez tenga un protocolo de comunicacion
+    # qué hace esto? nada. el protocolo no hace algo y no hay datos, pero puedes
+    # ir a una consola y escribir `telnet localhost 1079` y te dará un prompt que
+    # hace nada
+    fingerEndpoint = endpoints.serverFromString(reactor, 'tcp:1079')
+    fingerEndpoint.listen(FingerFactory())
     reactor.run()
 
 
